2fab827f9b4640dee112b1f97bf39883bdd179ef
Kass Daniello <zertroz@gmail.com>
2023-02-02 17:56:35 -0500

    Add start game to index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // This is where your project starts.
 const data = require('./src/data');
 const prototypeQuestions = data.prototypeData;
-const Game = require('./src/Game')
-const game = new Game()
-console.log('Your project is running...');
-game.start(prototypeQuestions)
+const Game = require('./src/Game');
+const game = new Game();
+game.start(prototypeQuestions);
